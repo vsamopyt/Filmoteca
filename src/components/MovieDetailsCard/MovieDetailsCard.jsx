d50b756027adfa0c277eb9ac8c238830c248d9ce
v.samopyt <v.samopyt14@gmail.com>
2024-08-19 15:18:05 +0300

    commit-07

--- src/components/MovieDetailsCard/MovieDetailsCard.jsx
+++ src/components/MovieDetailsCard/MovieDetailsCard.jsx
@@ -82,7 +82,7 @@ export default function MovieDetailsCard({ data }) {
               />
             ) : (
               <img
-                src="../"
+                src="../../../curtain-2757815_1280_1.png"
                 alt="blank-image"
               />
             )}
